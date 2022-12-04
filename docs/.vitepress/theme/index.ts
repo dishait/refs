0d35f2ae908937874c2eafd52e2b8a8a60c50271
markthree <1801982702@qq.com>
2022-12-04 20:28:28 +0800

    chore: component type confirmation

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,6 +1,11 @@
 import './cutom.css'
 import DefaultTheme from 'vitepress/theme'
+import Graph from './components/Graph.vue'
 
 export default {
-	...DefaultTheme
+	...DefaultTheme,
+	enhanceApp(ctx) {
+		DefaultTheme.enhanceApp(ctx)
+		ctx.app.component('Graph', Graph)
+	}
 }
